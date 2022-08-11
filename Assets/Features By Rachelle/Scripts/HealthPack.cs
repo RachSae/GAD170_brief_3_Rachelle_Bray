e45067751e6e1309497b635052e9e64801f7ee88
Rachelle <106799025+RachSae@users.noreply.github.com>
2022-08-11 14:50:17 +1000

    finished health pack an scene

--- Assets/Features By Rachelle/Scripts/HealthPack.cs
+++ Assets/Features By Rachelle/Scripts/HealthPack.cs
@@ -8,12 +8,13 @@ namespace RachelleBray
 
     public class HealthPack : MonoBehaviour
     {
+        [SerializeField] private MeshRenderer healthPackRenderer;
         private void OnTriggerEnter(Collider other)
         {
            if (other.CompareTag("Player"))
             {
                 other.GetComponent<Tank>().tankHealth.ApplyHealthChange(20);
-                
+                healthPackRenderer.material.color = Color.black;
             }
         }
     }
